8cbfce2fb4e39bc69f8d1f792a4acce2eb9a21b9
iamMwirigi <mwirigimburugu81@gmail.com>
2024-05-16 10:36:33 +0300

    databases

--- base/views.py
+++ base/views.py
@@ -1,25 +1,23 @@
 from django.shortcuts import render
 from .models import Room
 
-rooms = [
-    {'id': 1, 'name': 'Lets learn python!'},
-    {'id': 2, 'name': 'Design with me'},
-    {'id': 3, 'name': 'Frontend developers'},
-]
+# rooms = [
+#     {'id': 1, 'name': 'Lets learn python!'},
+#     {'id': 2, 'name': 'Design with me'},
+#     {'id': 3, 'name': 'Frontend developers'},
+# ]
 
 def home(request):
     rooms = Room.objects.all()
     context = {'rooms': rooms}
-    return render(request, 'home.html', context)
+    return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-            context = {'room': room}
-    return render(request, 'room.html')
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
 
 def server(request):
-    return render(request, 'server.html')  
+    context = {'server': server}
+    return render(request, 'base/server.html', context)  
