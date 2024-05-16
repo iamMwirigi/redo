4dfd76615e957fc3bad8af6547549d5770ff9c51
iamMwirigi <mwirigimburugu81@gmail.com>
2024-05-16 14:53:37 +0300

    crud

--- base/views.py
+++ base/views.py
@@ -18,6 +18,10 @@ def room(request, pk):
     context = {'room': room}
     return render(request, 'base/room.html', context)
 
+def createRoom(request):
+    context = {}
+    return render(request, 'base/room_form.html', context)
+
 def server(request):
     context = {'server': server}
     return render(request, 'base/server.html', context)  
